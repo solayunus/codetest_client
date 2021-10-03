5e16ba008226da7e47bffedc407ef40bfa790e05
Yunus O. KAREEM <solayunus19@gmail.com>
2021-10-03 21:22:26 +0100

    basic api and frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      textData:[]
+    };
+}
+  componentDidMount() {
+
+    fetch('http://localhost:5000/api/get').then(respone =>
+      respone.json())
+      .then((data => this.setState({ textData: data })))
+  }
+
+
+  render() {
+
+
+    return <div className="App-header" >{this.state.textData.map(text => (
+      <p key={text.id}>
+
+        {text.text}
+
+      </p>
+
+    ))}</div>;
+  }
 }
 
-export default App;
+export default App
+
+
