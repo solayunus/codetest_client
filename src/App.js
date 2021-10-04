920f45d06379d558bcb287f5ae7e91d06fc12a11
Yunus O. KAREEM <solayunus19@gmail.com>
2021-10-04 10:14:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,25 +6,24 @@ import React, { Component } from 'react'
 
 
 export class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      textData:[]
+      textData: [],
     };
-}
-  componentDidMount() {
-
-    fetch('http://localhost:5000/api/get').then(respone =>
-      respone.json())
-      .then((data => this.setState({ textData: data })))
   }
 
+  //https://codetestbackendapi.herokuapp.com/api/get
+  //http://localhost:5000/api/get
+
+  componentDidMount() {
+    fetch('https://codetestbackendapi.herokuapp.com/api/get')
+      .then(respone => respone.json())
+      .then(data => this.setState({ textData: data }));
+  }
 
   render() {
-
-
     return (
       <div className='App-header'>
         {this.state.textData.map(text => (
